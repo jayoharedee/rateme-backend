16c4732b298551eaf82893384f10526066ec510f
Jordan Michael <jordanm@tuta.io>
2019-11-04 11:14:42 -0500

    validation pipes for feedback

--- src/feedback/dto/filter-feedback.dto.ts
+++ src/feedback/dto/filter-feedback.dto.ts
@@ -1,6 +1,16 @@
 import { FeedbackStatus } from "../feedback.model";
+import { IsOptional, IsNotEmpty, IsIn } from "class-validator";
 
 export class FilterFeedbackDto {
+  @IsOptional()
+  @IsIn([
+    FeedbackStatus.OPEN,
+    FeedbackStatus.IN_PROGRESS,
+    FeedbackStatus.DONE,
+  ])
   status: FeedbackStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
   search: string;
 }
