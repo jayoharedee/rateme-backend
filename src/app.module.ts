e691262479b42abe366a532370f38c2946cf264b
Jordan Michael <jordanm@tuta.io>
2019-11-08 10:41:31 -0500

    imported typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FeedbackModule } from './feedback/feedback.module';
-import { ConfigModule } from './config/config.module';
+// import { ConfigModule } from './config/config.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+// import { ConfigService } from './config/config.service';
 
 @Module({
-  imports: [FeedbackModule, ConfigModule, TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), FeedbackModule],
 })
 export class AppModule {}
