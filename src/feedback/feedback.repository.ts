898ff1c5b149b143acd61901a92d8e9f2689d1a6
Jordan Michael <jordanm@tuta.io>
2019-11-08 20:14:19 -0500

    createfeedback repository method

--- src/feedback/feedback.repository.ts
+++ src/feedback/feedback.repository.ts
@@ -1,8 +1,21 @@
 import { Repository, EntityRepository } from "typeorm";
 import { Feedback } from "./feedback.entity";
+import { CreateFeedbackDto } from "./dto/create-feedback.dto";
+import { FeedbackStatus } from "./feedback-status.enum";
 
 
 @EntityRepository(Feedback)
 export class FeedbackRepository extends Repository<Feedback> {
+  async createFeedback(createFeedbackDto: CreateFeedbackDto): Promise<Feedback> {
+    const { title, description } = createFeedbackDto
 
+    const feedback = new Feedback()
+    feedback.title = title
+    feedback.description = description
+    feedback.status = FeedbackStatus.OPEN
+
+    await feedback.save()
+
+    return feedback
+  }
 }
